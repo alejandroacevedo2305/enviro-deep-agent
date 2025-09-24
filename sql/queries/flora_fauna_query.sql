f2bafd332ddde0af5f33474cedb1886cd4ff57cd
alejandroacevedo2305 <a.acevedo.aracena@gmail.com>
2025-09-24 14:12:22 -0300

    query updated

--- sql/queries/flora_fauna_query.sql
+++ sql/queries/flora_fauna_query.sql
@@ -16,32 +16,47 @@ select
   df.file_name,
   df.from_compressed_file,
   concat_ws('-', p.id, el.type, el.subtype, 'file_id_' || df.id::text) as file_identifier,
-  replace(df.s3_url, 'https://nviro-crawlers.s3.us-west-2.amazonaws.com/', '') as s3_key,
-  replace(df.s3_url, 'https://nviro-crawlers.s3.us-west-2.amazonaws.com/', 'https://443073691211-rq2lkjfg.us-west-2.console.aws.amazon.com/s3/object/nviro-crawlers?region=us-west-2&bucketType=general&prefix=') as aws_url
+  df.s3_key,
+  concat('https://443073691211-rq2lkjfg.us-west-2.console.aws.amazon.com/s3/object/nviro-crawlers?region=us-west-2&bucketType=general&prefix=', df.s3_key) as aws_url
 from proyecto p
   join extracted_link el on p.id = el.fk_project_id
   join downloader.file df on el.id = df.fk_extracted_link_id
 where
+  -- filtro para priorizar proyectos con todos los docs descargados (quitar después)
   p.id in (
       select p.id--, p.nombre, bool_and(el.used) as ready, count(e.*) > 0 as has_errors
       from proyecto p
         join extracted_link el on el.fk_project_id = p.id
         left join downloader.error_log e on el.id = e.fk_extracted_link_id
-      where p.estado = 'Aprobado' -- solo proyectos aprobados de momento
-        and (
-          el.type like 'addendum-physics-files-%' or
-          el.type like 'complementary-addendum-physics-files-%' or
-          el.type = 'ei-document'
-        )
+      where (
+        (
+          p.estado = 'Aprobado' -- solo proyectos aprobados de momento
+          and (
+            el.type like 'addendum-physics-files-%' or
+            el.type like 'complementary-addendum-physics-files-%' or
+            el.type = 'ei-document' or
+            el.type = 'icsara' and el.subtype != 'desconocido'
+          )
+        ) or (
+          el.subtype = 'anticipated-finished-resolution'
+        ))
         and el.url != 'https://seia.sea.gob.cl/archivos/'
         and p.fecha_de_presentacion >= '2014-01-01'
       group by p.id, p.nombre
       having bool_and(el.used) = true and count(e.*) = 0
-  ) -- filtro para priorizar proyectos con todos los docs descargados
+  )
   and (
-    el.type like 'addendum-physics-files-%'
-    or el.type like 'complementary-addendum-physics-files-%'
-    or el.type = 'ei-document'
+    (
+      p.estado = 'Aprobado' -- solo proyectos aprobados de momento
+      and (
+        el.type like 'addendum-physics-files-%' or
+        el.type like 'complementary-addendum-physics-files-%' or
+        el.type = 'ei-document' or
+        el.type = 'icsara' and el.subtype != 'desconocido'
+      )
+    ) or (
+      el.subtype = 'anticipated-finished-resolution'
+    )
   )
   and el.url != 'https://seia.sea.gob.cl/archivos/'
   and el.used = true
